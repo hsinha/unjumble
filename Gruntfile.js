d50494321c316d42b6b059a646dded27793515fd
Harsh <hsinha@users.noreply.github.com>
2014-01-21 17:27:42 +0530

    Checkpoint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			sass: {
-				files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
+				files: ['scss/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
 				tasks: ['sass:dist']
 			},
 			livereload: {
@@ -16,12 +16,29 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				files: {
-					'public/css/styles.css': 'sass/styles.scss'
+					'public/css/app.css': 'scss/app.scss',
+					'public/css/foundation.css': 'scss/foundation.scss',
+					'public/css/normalize.css': 'scss/normalize.scss',
 				}
 			}
+		},
+
+		cssmin: {
+			minify: {
+
+				files:{
+
+					'public/css/foundation.min.css':'public/css/foundation.css',
+					'public/css/app.min.css':'public/css/app.css'
+
+
+				}
+
+			  }
 		}
 	});
-	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.registerTask('default', ['sass:dist','cssmin', 'watch']);
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
