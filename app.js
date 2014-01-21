0e6a4a9a396fbe95b8facb956c30915956dc8093
harsh <harsh@DEM2.(none)>
2014-01-21 15:38:16 +0530

    Foundation Skeleton created, going to use grunt to automate build of sass

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use("/js",express.static(__dirname+'/public/js'));
 app.use("/img",express.static(__dirname+'/public/img'));
 
 app.get('/', function(req, res){
-  res.render('index.hbs');
+  res.render('index.hbs',{
+  	'title':'UnJumble',
+  	'description':'desc',
+  	'version':'Version 0.0.9'
+  });
 });
 
 app.listen(3000);
